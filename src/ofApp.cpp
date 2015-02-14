e444157e88c395b5224205486a1e7e3873b7daf6
tomyhyde <hyde.tomy@gmail.com>
2015-02-14 16:34:24 +0900

    at Class

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,17 +1,81 @@
 #include "ofApp.h"
-
+#define NUM_OF_POLYGONS 200
 //--------------------------------------------------------------
 void ofApp::setup(){
+    
+    ofEnableDepthTest();//実際の描画上での距離感（深度）を実装
+    for (int i = 0; i< NUM_OF_POLYGONS; i++){
+        polygons.push_back(new Polygon3d); //push_back = 一番最後に入れるという意味
+        
+    }
+    for (int i = 0; i< NUM_OF_POLYGONS; i++){
+        cout << i <<";"<<
+                polygons.at(i)->mPolygonPosition <<endl;
+        //靴箱の１−２００番にきちんと生成されているかをcout<<endlで確認
+        //at polygonの計算式の中の(i)の数値を確認
+        //-> 計算式の中の項目を指定 ポインターを勉強しましょう　アロー演算子
+        //
+        
+        
+    }
+    
 
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+//    camera.setPosition(200*cos((double)ofGetElapsedTimeMillis()/500),
+//                       0,
+//                       200*sin((double)ofGetElapsedTimeMillis()/500));
+//    camera.lookAt(ofVec3f(0,0,0));
+    
+    
+    //半径１の円において
+    //(x,y)=(cosθ,sinθ)
+    //1秒で1000回回ると早すぎるので、/200(整数)することで速度をコントロール
+    //一定の整数で割り続けると少数が出てくるので、(double)をつけることで少数を処理できるようにする
+    //そうすると滑らかにカメラを回せる
+    //今の状態では半径１なので、200*をすることで円の半径を200にする
+    
+        camera.setPosition(3000*cos((double)ofGetElapsedTimeMillis()/2500),
+                           0,
+                           3000*sin((double)ofGetElapsedTimeMillis()/2500));
+        camera.lookAt(ofVec3f(0,0,0));
+    
 
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    ofBackground(0);
+    ofTranslate(ofGetWidth()/2, ofGetHeight()/2,0);
+
+    
+    camera.begin();
+    
+//    box.setPosition(0, 0, 0);
+//    box.set(100);
+//    ofSetColor(255);
+//    box.drawWireframe();
+    
+//    Polygon3d polygon1 = Polygon3d(); //ここでヘッダーファイルを初期化してる 新しく描きますよー
+//    polygon1.draw();
+    
+    for (int i = 9; i < NUM_OF_POLYGONS; i++) {
+        ofSetColor(ofRandom(255),ofRandom(255),ofRandom(255));
+        polygons.at(i)->draw();
+        ofSetColor(255, 255, 255);
+        //ofLine(ofVec3f(0, 0, 0), polygons.at(i)->mPolygonPosition);
+        //とりあえず中心からそれぞれの図形にラインを引きます
+        ofLine(polygons.at(i-1)->mPolygonPosition, polygons.at(i)->mPolygonPosition);
+        //これで形成された順番にそれぞれを線で結びます
+    }
+    
+    
+    camera.end();
+    
+    
+    
 
 }
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,6 +2,39 @@
 
 #include "ofMain.h"
 
+class Polygon3d { //新たにクラスを作る　クラスは型紙でここに道具を準備をしておく感覚　cppに書くのは内容
+public:
+    ofVec3f mPolygonPosition = ofVec3f(0,0,0); //どの位置にあるか
+    int mPolygonSize = 0; //どんな大きさか
+    int mPolygonType = 0; // 0:box, 1:Sphere どの形か
+    
+    Polygon3d(){
+        mPolygonPosition = ofVec3f(ofRandom(-2000,2000),
+                                   ofRandom(-2000,2000),
+                                   ofRandom(-2000,2000));
+        mPolygonSize = floor(ofRandom(10,100));
+        mPolygonType = floor(ofRandom(2)); //floorで小数点以下切捨て
+    
+    
+    
+    }; //constructor　書かないと動かない　初期化関数
+    ~Polygon3d(){}; //destructor
+    void draw(){
+        if (mPolygonType == 0){
+            ofBoxPrimitive boxPrimitive;  //BoxPrimitiveというクラス　すでに用意されている
+            boxPrimitive.setPosition(mPolygonPosition);
+            boxPrimitive.set(mPolygonSize);
+            boxPrimitive.drawWireframe();
+        } else if (mPolygonType == 1){
+            ofSpherePrimitive sphereprimitive;
+            sphereprimitive.setPosition(mPolygonPosition);
+            sphereprimitive.setRadius(mPolygonSize);
+            sphereprimitive.drawWireframe();
+        }
+    }
+};
+
+
 class ofApp : public ofBaseApp{
 
 	public:
@@ -19,4 +52,12 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 		
-};
+    ofBoxPrimitive box;
+    ofSpherePrimitive sphere;
+    ofEasyCam easycam;
+    ofCamera camera;
+    vector<Polygon3d *> polygons; //配列と同じように使える　番号のついた靴箱のようなもの
+    
+    
+    
+};;
